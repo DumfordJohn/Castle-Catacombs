a8c1e093a5955ac117a8b00675228834e5de7b11
dumforjh <dumforjh@mail.uc.edu>
2023-03-31 15:16:01 -0400

    Added the Pause Menu

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public Button resumeButton;
+
+    void Awake()
+    {
+        pauseMenu.SetActive(false);
+        resumeButton.onClick.AddListener(OnResumePressed);
+    }
+
+    void OnResumePressed()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +31,17 @@ public class Pause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Time.timeScale = 1;
     }
 }
