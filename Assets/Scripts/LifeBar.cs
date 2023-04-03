a6afb4961418081f92fc114d77c6c5894591d957
dumforjh <dumforjh@mail.uc.edu>
2023-04-03 13:54:02 -0400

    added back the ammo count

--- Assets/Scripts/LifeBar.cs
+++ Assets/Scripts/LifeBar.cs
@@ -7,16 +7,10 @@ public class LifeBar : MonoBehaviour
 {
     public Life targetLife;
     Image image;
-    // Start is called before the first frame update
 
     private void Awake()
     {
         image = GetComponent<Image>();
-    }
-
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
